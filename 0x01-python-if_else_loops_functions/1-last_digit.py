566890ed4d3f6ade5395254b88732b3a3436b7ca
Favour Orji <favouru077@gmail.com>
2022-06-01 05:39:44 -0700

    Conditional Statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ elif number > 0:
     last_number = number % 10
 
 if last_number > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_number))
+    print(f"Last digit of {number} is {last_number} and is greater than 5")
 elif last_number == 0:
     print("Last digit of {} is {} and is 0".format(number, last_number))
 elif last_number < 6 and last_number != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_number))
+    print(f"Last digit of {number} is {last_number} and is less than 6 and not 0")
