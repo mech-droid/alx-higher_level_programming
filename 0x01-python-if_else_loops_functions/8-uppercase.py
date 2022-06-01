6264cc603c48ebe9bbb93c65bb2e766551608987
Favour Orji <favouru077@gmail.com>
2022-06-01 15:09:36 -0700

    python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     if ord(str) >= ord('a') and ord(str) <= ord('z'):
-        print("{}".format((str) + 35))
+        print("{}".format((str) - 35))
     else:
         print("{}".format(str))
