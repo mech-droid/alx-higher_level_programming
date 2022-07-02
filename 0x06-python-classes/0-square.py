91c1a0c2da0475d1f2efadf33e6066f40c2627bc
Favour Orji <favouru077@gmail.com>
2022-07-02 03:22:52 -0700

    object

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A module containing the square class
+"""
 class Square:
-    """Represents an empty square."""
+    """
+    Represents an empty square.
+    """
     pass:
