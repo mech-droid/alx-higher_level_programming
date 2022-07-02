8fb532cff4d7fc7d2da752b4b47718deb485dea6
Favour Orji <favouru077@gmail.com>
2022-07-02 03:32:00 -0700

    object

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module containing the square class task
+"""
+class Square:
+    """
+    A square class for the alx project
+    """
+    def __init__(self, size):
+        """
+        Initializze the data
+	"""
+        self.__size=size
