84cb04a42fa9ef68983e861d1c546e880bf0ee44
Favour Orji <favouru077@gmail.com>
2022-07-02 03:48:51 -0700

    object

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module of OOp task 3
+"""
+class Square:
+    """
+    Creation of a new type Square
+    """
+    def __init__(self, size):
+        """
+        Initialization of the data size
+	"""
+	if type(size) != int:
+            raise TypeError("size must be an integer")
+	elif size < 0:
+            raise ValueError("size must be >= 0")
+	else:
+            self.__size=size
