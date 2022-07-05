9ef3f48b49d0c50748306fd335bf5c893580b3bb
Favour Orji <favouru077@gmail.com>
2022-07-04 19:13:29 -0700

    Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Task 1 of the module inheritance
+"""
+def lookup(obj):
+    """Returns the list ofattributes and method.
+    Args:
+         obj: object to lookup
+    """
+    return dir(obj)
