b75f51377ef1d980fe57110a7a70092422353f37
Favour Orji <favouru077@gmail.com>
2022-07-14 12:52:08 -0700

    List

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return 'None'
+    elif idx > len(my_list):
+        return 'None'
+    else:
+        idx = my_list.index(idx)
+        return idx
